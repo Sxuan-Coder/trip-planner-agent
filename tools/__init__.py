27ab68eb9cf6fad2a46a656867eea35b09148999
上玄 <1421148240@qq.com>
2025-09-15 16:45:00 +0800

    feat: 添加工具模块（天气查询和景点推荐）

---
+++ tools/__init__.py
@@ -0,0 +1,3 @@
+from .get_weather import get_weather
+from .get_attraction import get_attraction
+from .available_tools import available_tools
---
+++ tools/available_tools.py
@@ -0,0 +1,8 @@
+
+from tools import get_weather, get_attraction
+
+# 将所有工具函数放入一个字典，方便后续调用
+available_tools = {
+    "get_weather": get_weather,
+    "get_attraction": get_attraction,
+}
